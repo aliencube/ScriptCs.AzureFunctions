8a24a5fca24a95113c6f729de38e1e59342ef06e
Justin Yoo <justin.yoo@aliencube.com>
2016-08-29 01:44:09 +1000

    Fix appveyor.yml

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,6 +1,6 @@
 Param(
     [string] [Parameter(Mandatory=$true)] $Configuration,
-    [string] [Parameter(Mandatory=$false)] $Target = "net46"
+    [string] [Parameter(Mandatory=$false)] $TargetVersion = "net46"
 )
 
 $xunit = ".\packages\xunit.runner.console.2.1.0\tools\xunit.console.exe"
